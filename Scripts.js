afca13ebfdd58a02fe5decd4bf8793e83b68ce64
Start Coding Student 2 <session2@podium3mac.west-mec.local>
2015-10-22 18:09:10 -0700

    I added some variable to the Javascript.

--- Scripts.js
+++ Scripts.js
@@ -1,3 +1,7 @@
 /**
  * Created by session2 on 10/22/15.
  */
+var nameInput = document.getElementById('name');
+var descInput = document.getElementById("desc");
+var dateInput = document.getElementById("date");
+var LifeInput = document.getElementbyId("Life");
