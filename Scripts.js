9879f72ed512dc68b4b6e3124c2d06e5224eb626
Start Coding Student 2 <session2@podium3mac.west-mec.local>
2015-10-30 16:29:08 -0700

    I added some variables

--- Scripts.js
+++ Scripts.js
@@ -1,12 +1,29 @@
 /**
  * Created by session2 on 10/22/15.
  */
-var nameInput = document.getElementById('Name');
-var descInput = document.getElementById("Desc");
-var dateInput = document.getElementById("Date");
-var LifeInput = document.getElementById("Life");
-var ExpInput = document.getElementById("Exp");
+var nameInput = document.getElementById('name');
+var descInput = document.getElementById("desc");
+var dateInput = document.getElementById("date");
+var lifeInput = document.getElementById("life");
+var expInput = document.getElementById("exp");
 
 /*Get element by DOM */
     var tableOutput = document.getElementById("table");
 
+    function update() {
+        var newItem = getInputItem();
+        updateTable(newItem);
+    }
+
+function getInputItem() {
+    var item = {
+        name: nameInput.value,
+        desc: descInput.value,
+        date: dateInput.value,
+        life: lifeInput.value,
+        exp: expInput.value,
+    };
+    return item;
+}
+
+    function updateTable(item) {}
